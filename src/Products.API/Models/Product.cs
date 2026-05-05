3cf6315577cb0cea4dbc5e7514e272e6c0aef258
Belen Casal <belencasal@users.noreply.github.com>
2026-05-05 17:18:35 -0300

    agregar validaciones al modelo

--- src/Products.API/Models/Product.cs
+++ src/Products.API/Models/Product.cs
@@ -1,12 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
 namespace Products.API.Models;
 
 public class Product
 {
     public Guid Id { get; set; }
+
+    [Required]
+    [StringLength(100)]
     public string Nombre { get; set; } = "";
+
+    [StringLength(500)]
     public string? Descripcion { get; set; }
+
+    [Range(0.01, double.MaxValue)]
     public decimal Precio { get; set; }
+
+    [Range(0, int.MaxValue)]
     public int Stock { get; set; }
+
+    [Required]
     public string Categoria { get; set; } = "";
+
     public DateTime FechaCreacion { get; set; }
 }
